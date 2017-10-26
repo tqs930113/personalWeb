5a80239d228c4b08a90285eb51207d55f410aa58
tqs930113 <tqs930113@users.noreply.github.com>
2017-10-26 18:14:27 +0800

    modify the js

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 $(function(){
-	var scroll = $(document).scroll(function(event) {
-	var a = $('#lesson').offset();
-	var screen = $(window).height();
-	var documentH=$(document).height();
-		console.log($(this).scrollTop());
-		console.log('lesson offset'+a.top);
-		console.log(screen);
-		console.log(documentH);
-		// if(($(this).scrollTop())>a.top){
-		// 	$(this).scrollTop(a.top);
-		// }
-	});
+	// var scroll = $(document).scroll(function(event) {
+	// var a = $('#lesson').offset();
+	// var screen = $(window).height();
+	// var documentH=$(document).height();
+	// 	console.log($(this).scrollTop());
+	// 	console.log('lesson offset'+a.top);
+	// 	console.log(screen);
+	// 	console.log(documentH);
+	// 	// if(($(this).scrollTop())>a.top){
+	// 	// 	$(this).scrollTop(a.top);
+	// 	// }
+	// });
 });
